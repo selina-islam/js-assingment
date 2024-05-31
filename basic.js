77d6a1bedb1037e76887609699c3a9669ab31c94
selina-islam <digitalselina@gmail.com>
2024-05-31 11:09:04 +0600

    firt js assingment

---
+++ basic.js
@@ -0,0 +1,112 @@
+    // number 1
+
+
+
+let carName = "Volvo"
+console.log(carName);
+
+
+
+
+
+           // number 2
+
+//let firstName = "John"; let lastName = "Doe"; let age = 35;
+
+
+
+
+
+
+           //number 3
+
+/*x = 10;
+y = 5;
+x = x*y
+console.log(x); // x will be 50*/
+
+
+
+
+
+          //number 4
+
+/*let length = 16;    
+console.log(typeof(length));  //data type: number
+
+let lastName = "Johnson";
+console.log(typeof(lastName)); //data type: string 
+
+const x = {
+        firstName: "John",  
+        lastName: "Doe"
+      };
+      console.log(typeof(x));  //data type: object*/
+
+
+
+
+
+      //number 5
+
+/*&function myFunction(){
+  alert("Hello world!");
+}
+myFunction();*/
+
+
+
+
+    //number 6
+
+/*let  person = {
+  name: "John",
+  age: 50
+};
+alert(person.name + "is" + person.age);*/
+
+
+
+   
+   //number 7
+
+/*function myFunction(){
+  alert("Button Clicked");
+}*/
+
+
+
+
+   //number 8
+
+/*const cars = ["Volvo", "Jeep", "Mercedes"];
+console.log(cars.length);
+  
+const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+Brand[0] ="Ford";
+  console.log(Brand);*/
+
+
+
+     //number 9
+
+/*let math = Math.random();   
+console.log(math);  
+
+let a = Math.max(10, 20);
+console.log(a);
+
+let x = Math.sqrt(9);
+console.log(x);*/
+
+
+
+  //number 10
+
+/*let  x = 10;
+let y = 5;
+ alert(x > y);*/
+
+//  let age = 3;
+//  alert((age < 18)? "Too young": "Old enough")
+
